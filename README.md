3353e294985d15a868abd05f07580fb6d596bbb5
David Guan <DavidGuanDev@Gmail.com>
2017-03-13 21:50:14 +0800

    Support splunk doc, improve UI

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# search_splunk_answers
-Browser plugin for quick search in splunk answers
+### Browser plugin for quick search in splunk answers, splunk base, splunk docs
+![](https://cloud.githubusercontent.com/assets/10692276/23857072/cfea12e8-0836-11e7-9e8f-d3c0116f571f.png)
--- assets/popup.js
+++ assets/popup.js
@@ -1,23 +1,25 @@
 const SPLUNK_BASE_URL_PREFIX = 'https://splunkbase.splunk.com/apps/#/search/';
 const SPLUNK_ANWSERS_URL_PREFIX = 'https://answers.splunk.com/search.html?'
+const SPLUNK_DOC_PREFIX = 'https://docs.splunk.com/Special:SplunkSearch/docs?q=';
 
 document.addEventListener('DOMContentLoaded', () => {
   const submitButton = document.querySelector('#submit');
   const textInput = document.querySelector('#searchText');
-  const questionRadio = document.querySelector('#radioQuestion');
-  const appRadio = document.querySelector('#radioApp');
-  const topicRadio = document.querySelector('#radioTopic');
+  const typeSelector = document.querySelector('#selector');
 
   const handleSearch = () => {
     let url = 'SPLUNK_ANWSERS_URL_PREFIX';
     const searchText = textInput.value;
+    const type = typeSelector.value;
 
-    if (questionRadio.checked) {
+    if (type === 'question') {
       url = `${SPLUNK_ANWSERS_URL_PREFIX}q=${searchText}&type=question`;
-    } else if (appRadio.checked) {
+    } else if (type === 'app') {
       url = `${SPLUNK_BASE_URL_PREFIX}${searchText}`;
-    } else if (topicRadio.checked) {
+    } else if (type === 'topic') {
       url = `${SPLUNK_ANWSERS_URL_PREFIX}q=${searchText}&type=topic`;
+    } else if (type === 'doc') {
+      url = `${SPLUNK_DOC_PREFIX}${searchText}`;
     }
 
     window.open(url,'_blank');
